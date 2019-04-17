6e966a90eb085eb3a763b8bdcb0e40e1247587bc
apple <apple@TimdeMacBook-Pro.local>
2019-04-17 01:01:47 -0700

    Update command line functions

--- sshell.c
+++ sshell.c
@@ -1,20 +1,72 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
 
 struct CommandInput {
     char* command;
-    char* flag;//FIXME: change flag to a string instead of list
-    char** argument;
+    char** parsed_command;
 };
+
+char *read_command(int max_command_size){
+    char *command;
+    command = malloc(sizeof(char) * max_command_size);
+    if (command == NULL) {
+        fprintf(stdout, "Failed to allocate command");
+        exit(EXIT_FAILURE);
+    }
+    int input_character;
+    size_t command_length = 0;
+    while (EOF != (input_character = fgetc(stdin)) && input_character != '\n')
+    {
+        command[command_length + 1] = (char) input_character;
+        if (command_length == max_command_size)
+        {
+            fprintf(stdout, "command exceeds 512");
+            exit(EXIT_FAILURE);
+        }
+    }
+    command[command_length + 1] = '\0';
+    return command;
+}
+
+void display_prompt(){
+    fprintf(stdout, "sshell$ ");
+}
+
+void excecute(struct CommandInput user_command){
+    int status;
+    if (fork() == 0) {
+        /* Child */
+        //execvp();
+        perror("execvp");
+        exit(1);
+    }else{
+        /* Parent */
+        waitpid(-1, &status, 0);
+        printf("Child exited with status: %d\n", WEXITSTATUS(status));
+    }
+}
+
+struct CommandInput parse_command(char* command){
+    struct CommandInput user_command;
+    return user_command;
+}
 
 int main(int argc, char *argv[])
 {
-    char *cmd = (char*)malloc(512 * sizeof(char));
-    int retval;
-    printf("sshell$ ");
-    fgets(cmd, sizeof(char) * 512, stdin);
-    retval = system(cmd);
-    fprintf(stdout, "Return status value for '%s': %d\n", cmd, retval);
-    
+    struct CommandInput user_command;
+    /*user_command.command = (char*)malloc(512 * sizeof(char));
+     user_command.parsed_command = (char**)malloc(512 * sizeof(char*));
+     for (int i = 0; i < 512; i++) {
+     user_command.parsed_command[i] = (char*)malloc(10 * sizeof(char));
+     }*/
+    while (1) {
+        display_prompt();
+        char *command = read_command(512);
+        user_command = parse_command(command);
+        excecute(user_command);
+    }
     return EXIT_SUCCESS;
 }
---
+++ sshell/sshell/example.c
@@ -0,0 +1,9 @@
+//
+//  example.c
+//  sshell
+//
+//  Created by apple on 4/16/19.
+//  Copyright © 2019 Tim. All rights reserved.
+//
+
+#include "example.h"
