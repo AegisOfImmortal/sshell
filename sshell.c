19425a4d88b9610469611492cc77a6528f7055f6
apple <apple@TimdeMacBook-Pro.local>
2019-04-17 09:59:22 -0700

    Fixed bugs

--- sshell.c
+++ sshell.c
@@ -6,31 +6,34 @@
 #include <stdlib.h>
 
 struct CommandInput {
-    char command[512];
-    char parsed_command[512];
-    char * commands[16];
+    char * command;
+    char * parsed_command;
+    char ** commands;
     int length;
 };
 
+void executeBuiltin(struct CommandInput user_command);
+void executeCommand(struct CommandInput user_command);
+void printStatusMessage(char *command, int exitcode);
+
 int isBuiltin(char *command)
 {
-    if (strcmp("exit", command) == 0 || strcmp("cd", command) == 0 || strcmp("pwd", command == 0)) return 1;
+    if (strcmp("exit", command) == 0 || strcmp("cd", command) == 0 || strcmp("pwd", command) == 0)
+    {
+        return 1;
+    }
     return 0;
 }
 
 
 void read_command(struct CommandInput user_command){
-		fgets(user_command.command, 512, stdin);
-        user_command.length = strlen(userInput);
-        if (length >= 512)
-        {
-        	fprintf(stdout, "command is too long\n", );
-        	exit(EXIT_FAILURE);
-        }
-        while(length < 512) {
-            userInput[length] = '\0';
-        }
-        strcpy(user_command.parsed_command, user_command.command);
+    fgets(user_command.command, 512, stdin);
+    user_command.length = (int)strlen(user_command.command);
+    int filler = user_command.length - 1;
+    while(filler < 512) {
+        user_command.command[filler++] = '\0';
+    }
+    strcpy(user_command.parsed_command, user_command.command);
 }
 
 void display_prompt(){
@@ -46,39 +49,38 @@ void excecute(struct CommandInput user_command){
 }
 
 void parse_command(struct CommandInput user_command){
-	int i = 0;
-    user_command.commands = strtok(parsed_command, " ");
-    while (commands != NULL){
-        commands[i++] = arg;
+    char *arg = strtok(user_command.parsed_command, " ");
+    int i = 0;
+    while (arg != NULL){
+        user_command.commands[i++] = arg;
         arg = strtok(NULL, " ");
     }
-     while(i < 16) {
-        commands[i++] = '\0';
+    while(i < 16) {
+        user_command.commands[i++] = '\0';
     }
 }
 
 void executeBuiltin(struct CommandInput user_command)
 {
-
+    
     if(strcmp("exit", user_command.commands[0]) == 0) {
-    	free(user_command);
         fprintf(stderr, "Bye...");
         exit(0);
     } else if(strcmp("pwd", user_command.commands[0]) == 0) {
         char *dir = getcwd(NULL, 4096);
         fprintf(stdout, "%s\n", dir);
-
+        
         printStatusMessage(user_command.command, 0);
     } else if(strcmp("cd", user_command.commands[0]) == 0) {
         int status = chdir(user_command.commands[1]);
-
+        
         if(status != 0) {
             fprintf(stdout, "Error: no such directory\n");
             printStatusMessage(user_command.command, 1);
         } else {
             printStatusMessage(user_command.command, 0);
         }
-
+        
     }
 }
 
@@ -86,17 +88,17 @@ void executeCommand(struct CommandInput user_command)
 {
     // Create a new child process to run the command
     int pid = fork();
-
+    
     if(pid == 0) {
         // If the code is running in the original process, execute the command
-        execvp(user_command.commands[0], user_command.command);
-
+        execvp(user_command.commands[0], user_command.commands);
+        
         // Execution should never reach this point unless command is not found
-        fprintf(stderr, "Error: command not found\n");
-
+        fprintf(stderr, "Error: command %s not found\n",user_command.commands[0]);
+        
         // Return code of child process should be 1 if the command is not found
         exit(1);
-
+        
     } else if(pid > 0) {
         // If code is running in the child process, wait for the command to finish
         wait(&pid);
@@ -115,13 +117,18 @@ void printStatusMessage(char *command, int exitcode)
 
 int main(int argc, char *argv[])
 {
-    struct CommandInput user_command;
-    user_command = (char*)malloc(sizeof(struct CommandInput));
+    struct CommandInput *user_command = (struct CommandInput*)malloc(sizeof(struct CommandInput));
+    user_command->command = malloc (512 * sizeof (char));
+    user_command->parsed_command = malloc (512 * sizeof (char));
+    user_command->commands = (char**) malloc(16 * sizeof(char*));
+    for (int i = 0; i<16; i++) {
+        user_command->commands[i] = (char*) malloc(513*sizeof(char));
+    }
     while (1) {
         display_prompt();
-        read_command(user_command);
-        parse_command(user_command);
-        excecute(user_command);
+        read_command(*user_command);
+        parse_command(*user_command);
+        excecute(*user_command);
     }
     return EXIT_SUCCESS;
 }
